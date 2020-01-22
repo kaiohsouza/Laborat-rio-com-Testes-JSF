d61d461188d7e179e008bcbdcca1de253bdbf99b
Kaio Henrique <kaiohenry16@gmail.com>
2020-01-21 22:05:05 -0300

    criação do documento candlestick.java

---
+++ src/main/java/br/com/kaio/argentum/modelo/CandlestickFactory.java
@@ -0,0 +1,30 @@
+package br.com.kaio.argentum.modelo;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class CandlestickFactory {
+
+		public Candlestick geraCandlestickData(List<Negociacao> negociacoes, LocalDateTime data) {
+			
+			double abertura = negociacoes.get(0).getPreco();
+			double fechamento = negociacoes.get(negociacoes.size() - 1).getPreco();
+			
+			double minimo = negociacoes.get(0).getPreco();
+			double maximo = negociacoes.get(0).getPreco();
+			
+			double volume = 0;
+			for (Negociacao negociacao : negociacoes) {
+				volume += negociacao.getVolume();
+				
+				if(negociacao.getPreco() > maximo) {
+					maximo = negociacao.getPreco();
+				}else if (negociacao.getPreco() < minimo) {
+					minimo = negociacao.getPreco();
+				}
+			}
+			
+			return new Candlestick(abertura, fechamento, maximo, minimo, volume, data);
+		}
+	
+}
