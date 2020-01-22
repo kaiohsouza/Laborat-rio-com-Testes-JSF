7ce6951a0ff65e2053cc02c72d2bfeb959baacac
Kaio Henrique <kaiohenry16@gmail.com>
2020-01-21 22:06:41 -0300

    criacao do documento negociaçoes.java

---
+++ src/main/java/br/com/kaio/argentum/modelo/Negociacao.java
@@ -0,0 +1,37 @@
+package br.com.kaio.argentum.modelo;
+
+import java.time.LocalDateTime;
+
+public final class Negociacao {
+	
+	private final double preco;
+	private final int quantidade;
+	private final LocalDateTime data;
+
+
+	public Negociacao(double preco, int quantidade, LocalDateTime data) {
+		this.preco = preco;
+		this.quantidade = quantidade;
+		this.data = data;
+	}
+	
+	public double getPreco() {
+		return preco;
+	}
+
+	public int getQuantidade() {
+		return quantidade;
+	}
+
+
+	public LocalDateTime getData() {
+		return data;
+	}
+	
+	public double getVolume() {
+			return (this.preco * this.quantidade);	
+	}
+	
+	
+
+}
